0bddc682f6a5546fbb41fd5ab6caa630a9a65c67
karl <karl+github@la-grange.net>
2012-07-05 23:32:51 -0400

    Adding a few more tests about the format of HTTP uris

--- tests/survey_tests.py
+++ tests/survey_tests.py
@@ -10,6 +10,30 @@ class UriCheckTests(unittest.TestCase):
         httpuristart = 'http://www.opera.com/'
         self.assertTrue(gooduri.ishttpURI(httpuristart))
 
+    def testHttpsURI(self):
+        gooduri = core.UriCheck()
+        httpuristart = 'https://www.opera.com/'
+        self.assertTrue(gooduri.ishttpURI(httpuristart))
+
+    def testUpperCaseHttpURI(self):
+        gooduri = core.UriCheck()
+        httpuristart = 'HTTP://www.opera.com/'
+        self.assertTrue(gooduri.ishttpURI(httpuristart))
+
+    def testHttpURIWrong(self):
+        gooduri = core.UriCheck()
+        httpurifalse = 'mailto:foo@example.org'
+        self.assertFalse(gooduri.ishttpURI(httpurifalse))
+
+    def testLeadingSpaceHttpURI(self):
+        gooduri = core.UriCheck()
+        httpuristart = '  http://www.opera.com/'
+        self.assertTrue(gooduri.ishttpURI(httpuristart))
+
+    def testTrailingSpaceHttpURI(self):
+        gooduri = core.UriCheck()
+        httpuristart = 'http://www.opera.com/   '
+        self.assertTrue(gooduri.ishttpURI(httpuristart))
 
 
 def main():
